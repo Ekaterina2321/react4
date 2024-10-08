ecd45c83c2b0b6d851626986aa011274893ea05d
Ekaterina2321 <ekaterina_sandakova2@mail.ru>
2024-10-08 16:40:53 +0300

    Практика 4 react

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,11 @@
+import { Banner } from "../../components/Banner/Banner";
+
+export function HomePage() {
+    return ( 
+        <section>
+            <Banner/>
+
+            <h2>Главная</h2>
+        </section>
+    )
+}
